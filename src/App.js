43b098f2d5e4c21308de6120dd2790f5da506fcc
Arindam Dutta <arindam408@gmail.com>
2021-03-26 01:09:04 +0530

    pod complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
+import React from "react";
+import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
+import Home from "./components/Home";
+import Search from "./components/Search";
 
+import './App.css';
 
 function App() {
+
+  //Enter your key
+  const passkey = "wd1pwb5i97xsD3FW7gmA0Z4cnvCuq5AKPF7obrua";
+
   return (
-    <div className="App">
-     hello
+    <div id="screen">
+      <BrowserRouter>
+        <header>
+          <h4>NASA Media Search</h4>
+        </header>
+        <div id="nav">
+          <Link to="/" className='nav-items'>Home</Link>
+          <input className='nav-items'/>
+          <button className='nav-items'>Search</button>
+        </div>
+        <div id='container'>
+
+        <Switch>
+          <Route path="/Search">
+            <Search />{" "}
+          </Route>
+          <Route path="/">
+            <Home passkey={passkey} />
+          </Route>
+        </Switch></div>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import axios from "axios";
+import './Home.css';
+
+
+export default function Home(props) {
+  const [title, setTitle] = React.useState("");
+  const [image, setImage] = React.useState("");
+  const [date, setDate] = React.useState("");
+  const [description, setDescription] = React.useState("");
+  
+
+   let url=`https://api.nasa.gov/planetary/apod?api_key=${props.passkey}`;
+  axios
+    .get(url
+    )
+    .then((res) => {
+      setDate(res.data.date);
+      setTitle(res.data.title);
+      setImage(res.data.url);
+      setDate(res.data.date);
+      setDescription(res.data.explanation);
+    });
+
+  return (
+    <div id="home-container">
+        <div id='title-container'><b>Title : </b>   {title}</div>
+
+      <div id="image-container">
+        <img src={image} id="image" />
+      </div>
+      <div id='description-container'>
+         <b>Description : </b>  {description}</div>
+      <div id='date-container'>{date}</div>
+    </div>
+  );
+}
